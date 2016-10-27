3bb4dc8c4689a785255ca1bc35bd16bd3013cf09
jcincotta22 <jcincotta22@gmail.com>
2016-10-27 17:37:01 -0400

    more tests and some styling

--- react/src/components/Ticket.js
+++ react/src/components/Ticket.js
@@ -535,7 +535,7 @@ class Ticket extends Component {
       <div className='search-history-div'><a href="#" onClick={this.handleDeleteSearch} className='link'>Clear History</a></div>{clickedSearchHistoriesDatas}</div>
     }else{
       clickedOutput = <div className="clicked col-lg-12 col-md-12 col-sm-12 col-xs-12">
-      <p className='intro'>Search for your favorite artist above. Enter a date range for the event.<br/>Entering a zip code will retrieve recommended events near you!</p>
+      <p className='intro'>Search for your favorite artist above.<br/>Enter a date range for the event.<br/>Entering a zip code will retrieve recommended events near you!</p>
       <img src='http://www.whitneypeakhotel.com/images/scrollilax/frame7_i2.png?' className='main-img'/>
       </div>
     }
@@ -581,7 +581,7 @@ class Ticket extends Component {
                 </div>
               </div>
               <div className="panel panel-info">
-                <div className="panel-heading"><h4>Recommended By Us</h4></div>
+                <div className="panel-heading"><h4 className='text'>Recommended By Us</h4></div>
                 <div id='recommended' className='results'>
                   {recommendedDatas}
                 </div>
@@ -597,7 +597,7 @@ class Ticket extends Component {
             <div className='col-lg-3 col-md-3 col-sm-12 col-xs-12'>
               <div className='search-history-div'><a href="#" onClick={this.handleClickSearch} className='link'>Search History</a></div>
               <div className="panel panel-info">
-                <div className="panel-heading"><h4>Saved Event Bucket</h4></div>
+                <div className="panel-heading"><h4 className='text'>Saved Event Bucket</h4></div>
                 <div className="panel-info">{savedEventsDatas}</div>
               </div>
             </div>
--- spec/features/saved_events_controller_spec.rb
+++ spec/features/saved_events_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Api::SavedEventsController, type: :controller do
   feature "create saved event" do
-    scenario 'creates a new SavedEvent if user is signed in' do
+    scenario 'creates a new SavedEvent from bandsInTown if user is signed in' do
       user = FactoryGirl.create(:user)
       allow(request.env['warden']).to receive(:authenticate!).and_return(user)
       allow(controller).to receive(:current_user).and_return(user)
@@ -13,7 +13,46 @@ describe Api::SavedEventsController, type: :controller do
       expect(res_body['message']).to eq("Event was saved to your Event Bucket")
       expect(res_body['message']).not_to be("User must be signed in to save this event")
 
-      saved_event = SavedEvent.first
+      saved_event = SavedEvent.last
+      expect(saved_event.user_id).to eq(user.id)
+    end
+
+    scenario 'creates a new SavedEvent from bandsInTown if user is signed in' do
+      post :create, params: { saved_event: { date: '2016-10-31', site: 'bandsInTown', keyword: 'Adele', title: 'Adele', url: 'www.tickets.com' } }
+      expect(response.status).to eq(200)
+      res_body = JSON.parse(response.body)
+      expect(res_body['message']).not_to eq("Event was saved to your Event Bucket")
+      expect(res_body['message']).to eq("User must be signed in to save this event")
+    end
+
+
+    scenario 'creates a new SavedEvent from ticketmaster if user is signed in' do
+      user = FactoryGirl.create(:user)
+      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+      allow(controller).to receive(:current_user).and_return(user)
+      login_as(user, :scope => :user)
+      post :create, params: { saved_event: { date: '2016-10-31', site: 'ticketmaster', keyword: 'Adele', title: 'Adele', url: 'www.tickets.com' } }
+      expect(response.status).to eq(200)
+      res_body = JSON.parse(response.body)
+      expect(res_body['message']).to eq("Event was saved to your Event Bucket")
+      expect(res_body['message']).not_to be("User must be signed in to save this event")
+
+      saved_event = SavedEvent.last
+      expect(saved_event.user_id).to eq(user.id)
+    end
+
+    scenario 'creates a new SavedEvent from seatgeek if user is signed in' do
+      user = FactoryGirl.create(:user)
+      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+      allow(controller).to receive(:current_user).and_return(user)
+      login_as(user, :scope => :user)
+      post :create, params: { saved_event: { date: '2016-10-31', site: 'seatGeek', keyword: 'Adele', title: 'Adele', url: 'www.tickets.com' } }
+      expect(response.status).to eq(200)
+      res_body = JSON.parse(response.body)
+      expect(res_body['message']).to eq("Event was saved to your Event Bucket")
+      expect(res_body['message']).not_to be("User must be signed in to save this event")
+
+      saved_event = SavedEvent.last
       expect(saved_event.user_id).to eq(user.id)
     end
   end
